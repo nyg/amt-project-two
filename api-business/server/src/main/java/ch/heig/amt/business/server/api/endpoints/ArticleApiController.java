bcc2ae3aa28cb11587c22c180e575bd6392fc5fd
SamuelMettler <samuel@mettlerpm.com>
2020-01-11 17:33:51 +0100

    post /article done

--- api-business/server/src/main/java/ch/heig/amt/business/server/api/endpoints/ArticleApiController.java
+++ api-business/server/src/main/java/ch/heig/amt/business/server/api/endpoints/ArticleApiController.java
@@ -1,20 +1,29 @@
 package ch.heig.amt.business.server.api.endpoints;
 
+import ch.heig.amt.business.server.api.exceptions.AuthenticationException;
 import ch.heig.amt.business.server.api.model.Article;
 import ch.heig.amt.business.server.api.model.OptionalArticle;
+import ch.heig.amt.business.server.entities.ArticleEntity;
+import ch.heig.amt.business.server.repositories.ArticleRepository;
+import ch.heig.amt.business.server.service.AccessGranted;
+import ch.heig.amt.business.server.service.AuthenticationService;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ch.heig.amt.business.server.api.ArticleApi;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.net.URI;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-12-16T15:02:02.960Z")
 
@@ -24,6 +33,13 @@ public class ArticleApiController implements ArticleApi {
     private static final Logger log = LoggerFactory.getLogger(ArticleApiController.class);
 
     private final ObjectMapper objectMapper;
+    @Autowired
+    AccessGranted accessGranted;
+    @Autowired
+    AuthenticationService authenticationService;
+    @Autowired
+    ArticleRepository articleRepository;
+
 
     private final HttpServletRequest request;
 
@@ -39,7 +55,7 @@ public class ArticleApiController implements ArticleApi {
     }
 
     public ResponseEntity<Article> articlePost(@ApiParam(value = "" ,required=true )  @Valid @RequestBody Article article) {
-        String accept = request.getHeader("Accept");
+       /* String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
                 return new ResponseEntity<Article>(objectMapper.readValue("{  \"price\" : 6.02745618307040320615897144307382404804229736328125,  \"name\" : \"name\",  \"description\" : \"description\",  \"id\" : 0}", Article.class), HttpStatus.NOT_IMPLEMENTED);
@@ -49,7 +65,38 @@ public class ArticleApiController implements ArticleApi {
             }
         }
 
-        return new ResponseEntity<Article>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<Article>(HttpStatus.NOT_IMPLEMENTED);*/
+       if(accessGranted.granted(request)){
+           String authorization = request.getHeader("Authorization");
+           String tokenValue = authorization.split(" ")[1];
+           DecodedJWT token = authenticationService.verifyToken(tokenValue);
+           if (token == null) {
+               throw new AuthenticationException();
+           }
+
+           boolean admin = token.getClaim("admin").asBoolean();
+           //TODO : if admin not true launch exception
+
+           ArticleEntity newArticleEntity = toArticleEntity(article);
+           articleRepository.save(newArticleEntity);
+
+           URI location = ServletUriComponentsBuilder
+                   .fromCurrentRequest().path("/{id}")
+                   .buildAndExpand(newArticleEntity.getId()).toUri();
+
+           return ResponseEntity.created(location).build();
+       }
+
+       return null;
+    }
+
+    private ArticleEntity toArticleEntity(Article article) {
+        ArticleEntity entity = new ArticleEntity();
+        entity.setDescription(article.getDescription());
+        entity.setId(article.getId());
+        entity.setName(article.getName());
+        entity.setPrice(article.getPrice());
+        return entity;
     }
 
     public ResponseEntity<Article> articlePut(@ApiParam(value = "" ,required=true )  @Valid @RequestBody OptionalArticle article) {
