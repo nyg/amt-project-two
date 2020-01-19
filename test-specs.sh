14aa547be065aa6af9e1f1cd7d48b3faf8e65338
SamuelMettler <samuel@mettlerpm.com>
2020-01-20 00:08:23 +0100

    don't look at the time, it's clearly still sunday (error on one script)

--- test-specs.sh
+++ test-specs.sh
@@ -7,5 +7,5 @@ rootdir=$PWD
 cd "$rootdir"/api-user-mgmt/specs
 mvn clean package -Pcustom -DAPI_MGMT_HOST=localhost -DAPI_MGMT_PORT=8898
 
-#cd "$rootdir"/api-business/specs
-#mvn clean package -Pcustom -DAPI_BUSINESS_HOST=localhost -DAPI_BUSINESS_PORT=8898
+cd "$rootdir"/api-business/specs
+mvn clean package -Pcustom -DAPI_BUSINESS_HOST=localhost -DAPI_BUSINESS_PORT=8898
